01eba2ac56bc5e5dac3dad1f16551e2069cb44da
Matthew Krog <matthew@getsmarter.co.za>
2015-03-02 08:18:28 +0200

    Refactored the rna to dna complement, using a hash to perform the conversion

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,27 +1,17 @@
 class Complement
+  DNA_TO_RNA_DICTIONARY = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
 
-  def self.of_dna dna_string
-    array1 = ['G', 'C', 'T', 'A']
-    array2 = ['C', 'G', 'A', 'U']
-
-    rna_string = ""
-    dna_string.length.times do |i|
-      index = array1.index(dna_string[i])
-      rna_string += array2[index]
-    end
-    rna_string
+  def self.of_dna(dna_string)
+    convert(dna_string, DNA_TO_RNA_DICTIONARY)
   end
 
   def self.of_rna rna_string
-    array1 = ['G', 'C', 'T', 'A']
-    array2 = ['C', 'G', 'A', 'U']
-
-    dna_string = ""
-    rna_string.length.times do |i|
-      index = array2.index(rna_string[i])
-      dna_string += array1[index]
-    end
-    dna_string
+    convert(rna_string, DNA_TO_RNA_DICTIONARY.invert)
   end
 
+  def self.convert(initial_strand, dictionary)
+    output = ""
+    initial_strand.length.times { |i| output += dictionary[initial_strand[i]] }
+    output
+  end
 end
